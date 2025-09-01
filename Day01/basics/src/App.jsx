14dfff47b8a12af899e204d0d51a8489c5b0a64a
Parikshit <parikshitpandey03@gmail.com>
2025-09-01 22:34:38 +0530

    first day

---
+++ Day01/basics/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Hello from "./Hello";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-xl font-medium mt-10 mb-10 text-center">Learning Basic React</h1>
+        <Hello />
+      </div>
+       
+    </>
+  )
+}
+
+export default App
